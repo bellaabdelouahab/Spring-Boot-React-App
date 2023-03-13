5cdcfbaf61fce4f8b884b239bf844dc61a8953f8
Abdelouahab <abdobella977@gmail.com>
2023-03-14 00:38:11 +0100

    connect backend to front in build

--- src/main/java/com/nfs/project/config/SecurityConfiguration.java
+++ src/main/java/com/nfs/project/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.nfs.project.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -26,29 +27,31 @@ public class SecurityConfiguration {
   private final AuthenticationProvider authenticationProvider;
   private final LogoutHandler logoutHandler;
 
+  
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     
-    http.cors();
-    http
-        .csrf().disable()
-        .authorizeHttpRequests()
-        // .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-        .requestMatchers("/api/v1/auth/**").permitAll()
-        .requestMatchers("/api/v1/app/**").permitAll()
-        // .requestMatchers("/api/v1/app/**").authenticated()
-        .anyRequest().denyAll()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authenticationProvider(authenticationProvider)
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-        .logout()
-        .logoutUrl("/api/v1/auth/logout")
-        .addLogoutHandler(logoutHandler)
-        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
-    return http.build();
+      http.cors();
+      http
+          .csrf().disable()
+          .authorizeHttpRequests()
+          .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+          .requestMatchers("/api/v1/auth/**").permitAll()
+          .requestMatchers("/api/v1/app/**").permitAll()
+          // .requestMatchers("/api/v1/app/**").authenticated()
+          .anyRequest().denyAll()
+          .and()
+          .sessionManagement()
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          .and()
+          .authenticationProvider(authenticationProvider)
+          .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+          .logout()
+          .logoutUrl("/api/v1/auth/logout")
+          .addLogoutHandler(logoutHandler)
+          .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+      return http.build();
   }
 
 }
