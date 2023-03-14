61e2b09108c7659113f6eb4c58abbea7074250f4
bella abdelouahab <abdobella977@gmail.com>
2023-03-14 17:45:11 +0100

    add error handling for all token errors

--- src/main/java/com/nfs/project/exception/ExceptionFilterHandler.java
+++ src/main/java/com/nfs/project/exception/ExceptionFilterHandler.java
@@ -1,41 +1,62 @@
 package com.nfs.project.exception;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Data;
-
+import java.time.ZonedDateTime;
 import java.io.IOException;
-
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class ExceptionFilterHandler {
 
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter
-            .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+    private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm Z");
 
     // handle filter exception
     public void handleFilterException(HttpServletResponse response, Exception ex) throws ServletException, IOException {
-       if (ex instanceof ExpiredJwtException) {
-        // return json response containing ResponseDetails
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.getWriter().write(
-        new ResponseEntity<>(new ResponseDetails(
-                LocalDateTime.now().format(dateTimeFormatter),
-                HttpStatus.UNAUTHORIZED.value(),
-                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
-                ex.getMessage(),
-                "/api/v1/auth/login"
-        ), HttpStatus.UNAUTHORIZED).toString());
 
-
-       }else{
-        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+       if (ex instanceof io.jsonwebtoken.MalformedJwtException){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            System.out.println("expired"+HttpStatus.UNAUTHORIZED.value());
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonString = mapper.writeValueAsString(
+                new ResponseDetails(
+                            fmt.format(ZonedDateTime.now()),
+                            HttpStatus.UNAUTHORIZED.value(),
+                            HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                            ex.getMessage(),
+                            "/api/v1/auth/login"
+                    ));
+            response.getWriter().write(jsonString);
        }
+       else if (ex instanceof ExpiredJwtException){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write(new ObjectMapper().writeValueAsString(
+                new ResponseDetails(
+                            fmt.format(ZonedDateTime.now()),
+                            HttpStatus.UNAUTHORIZED.value(),
+                            HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                            ex.getMessage(),
+                            "/api/v1/auth/login"
+                ))
+            );
+        }
+        else if (ex instanceof UnsupportedJwtException){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write(new ObjectMapper().writeValueAsString(
+                    new ResponseDetails(
+                            fmt.format(ZonedDateTime.now()),
+                            HttpStatus.UNAUTHORIZED.value(),
+                            HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                            ex.getMessage(),
+                            "/api/v1/auth/login"
+                    )
+            ));
+        }
     }
 
     @Data
