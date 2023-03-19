abe8e6476b0332a3b4324963c5c738bc982137c3
y-mahdi <youssefmahdi.edu@gmail.com>
2023-03-19 02:39:10 +0000

    --please Make Sure add order at React is Working -- General Update

---
+++ react-app/src/Api/OrderBasketApi.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import config from "./ConfigApi";
+import { useState } from "react";
+
+const getOrderBasketByOrderNumber=(id)=>{
+    return new Promise((resolve,reject)=>{
+        axios.get(`${config.getApiPath()}/api/v1/app/orderBasket/orderNumber/${id}`).then((res)=>{
+            resolve(res.data)
+        }).catch((e)=>{
+            reject(e)
+        })
+    });
+}
+const getAllOrderBasketData=()=>{
+    return new Promise((resolve,reject)=>{
+        axios.get(`${config.getApiPath()}/api/v1/app/orderBasket`).then((res)=>{
+            resolve(res.data)
+        }).catch((e)=>{
+            reject(e)
+        })
+    });
+}
+const AddBasket=(customerId,productId,priceperunit,quantity)=>{
+    return new Promise((resolve,reject)=>{
+        axios.post(`${config.getApiPath()}/api/v1/app/orderBasket`,{
+            "orderId":customerId,
+            "productId":productId,
+            "priceperunit":priceperunit,
+            "quantity":quantity
+        }).then((res)=>{
+            resolve(res.data)
+        }).catch((e)=>{
+            reject(e)
+        })
+    })
+}
+
+
+const OrderBasketApi={getOrderBasketByOrderNumber,getAllOrderBasketData,AddBasket}
+
+export default OrderBasketApi;
